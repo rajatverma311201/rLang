760574a4d263e6b1da089c19b332fc231680cd84
Rajat Verma <rajat311201@gmail.com>
2024-08-10 23:23:25 +0530

    Token Model

---
+++ src/main/java/in/rajat/rLang/models/Token.java
@@ -0,0 +1,21 @@
+package in.rajat.rLang.models;
+
+import in.rajat.rLang.enums.TokenType;
+
+public class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    public Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
